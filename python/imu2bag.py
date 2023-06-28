e52bbda7787faa0bb25f5afcd2292a913e640f8f
zzzzyp-sgg <113188120+zzzzyp-sgg@users.noreply.github.com>
2023-06-28 11:40:54 +0800

    Update imu2bag.py
    delete findFiles

--- python/imu2bag.py
+++ python/imu2bag.py
@@ -7,48 +7,6 @@ from sensor_msgs.msg import Imu
 import rospy
 from geometry_msgs.msg import Vector3
 
-def findFiles(root_dir, filter_type, reverse=False):
-    """
-    在指定目录查找指定类型文件 -> paths,names,files
-    :param root_dir: 查找目录
-    :param filter_type: 文件类型
-    :param reverse: 是否返回倒序文件列表,默认为False
-    :return: 路径、名称、文件全路径
-    """
-
-    separator = os.path.sep
-    paths = []
-    names = []
-    files = []
-    for parent, dirname, filenames in os.walk(root_dir):
-        for filename in filenames:
-            if filename.endswith(filter_type):
-                paths.append(parent + separator)
-                names.append(filename)
-    for i in range(paths.__len__()):
-        files.append(paths[i] + names[i])
-    print(names.__len__().__str__() + " files have been found.")
-    
-    paths = np.array(paths)
-    names = np.array(names)
-    files = np.array(files)
-
-    index = np.argsort(files)
-
-    paths = paths[index]
-    names = names[index]
-    files = files[index]
-
-    paths = list(paths)
-    names = list(names)
-    files = list(files)
-    
-    if reverse:
-        paths.reverse()
-        names.reverse()
-        files.reverse()
-    return paths, names, files
-
 def readIMU(imu_path):
     timestamps = []
     wxs = []
